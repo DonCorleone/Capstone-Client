cfe25b29f6f9fdd262f595f1154b781ef1900500
DonCorleone <vitocorleone77@gmail.com>
2019-04-14 01:05:17 +0200

    all done first draft

--- src/app/components/cart-page/cart-page.component.ts
+++ src/app/components/cart-page/cart-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { ICartItem } from 'src/app/models/cart-item';
 
 @Component({
   selector: 'app-cart-page',
@@ -13,8 +14,32 @@ export class CartPageComponent implements OnInit {
   }
 
   cart = this.cartService.cart;
+  subTotal = this.cartService.subTotal;
+  shipping = this.cartService.shipping;
+  taxRate = this.cartService.taxRate;
+  tax = this.cartService.tax;
+  cartTotal = this.cartService.cartTotal;
 
   ngOnInit() {
+  }
+
+  onDecrementAmount(cartItem: ICartItem) {
+    this.cartService.decrementAmount(cartItem);
+  }
+  onIncrementAmount(cartItem: ICartItem) {
+    this.cartService.incrementAmount(cartItem);
+  }
+
+  onRemoveEntry(cartItem: ICartItem) {
+    this.cartService.removeEntry(cartItem);
+  }
+
+  onRecalculate() {
+    this.subTotal = this.cartService.subTotal;
+    this.shipping = this.cartService.shipping;
+    this.taxRate = this.cartService.taxRate;
+    this.tax = this.cartService.tax;
+    this.cartTotal = this.cartService.cartTotal;
   }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -30,8 +30,47 @@ export class CartService {
 
   constructor() { }
 
-  getAmountTotal(): number {
+  public get subTotal(): number {
     return this._cart.reduce((sum, current) => sum + current.total, 0);
+  }
+
+  public get shipping(): number {
+    return 10.00;
+  }
+
+  public get taxRate(): number {
+    return 10;
+  }
+
+  public get tax(): number {
+    return (this.taxRate / 100) * (this.subTotal + this.shipping);
+  }
+
+  public get cartTotal(): number {
+    return (this.subTotal + this.tax + this.shipping);
+  }
+
+  public decrementAmount(cartItem: ICartItem) {
+    // const tempItem = this._cart.find(entry => entry.name === cartItem.name);
+    if (cartItem.amount === 1) {
+      this.removeEntry(cartItem);
+    } else {
+      cartItem.amount --;
+      cartItem.total -= cartItem.price;
+    }
+  }
+  public incrementAmount(cartItem: ICartItem) {
+    // const tempItem = this._cart.find(entry => entry.name === cartItem.name);
+    cartItem.amount ++;
+    cartItem.total += cartItem.price;
+  }
+
+  public removeEntry(cartItem: ICartItem) {
+   // const tempItem = this._cart.find(entry => entry.name === cartItem.name);
+   const index = this._cart.indexOf(cartItem, 0);
+   if (index > -1) {
+    this._cart.splice(index, 1);
+   }
   }
 
   createOrUpdateCartItem(item: IItem, amount: number = 1) {
