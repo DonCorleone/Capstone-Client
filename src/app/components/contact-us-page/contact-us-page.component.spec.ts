1c130546d5fed25519089171acbfe51e4887ff02
Linus <vitocorleone77@gmail.com>
2020-12-06 21:17:06 +0100

    ng11

--- src/app/components/contact-us-page/contact-us-page.component.spec.ts
+++ src/app/components/contact-us-page/contact-us-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ContactUsPageComponent } from './contact-us-page.component';
 
@@ -6,7 +6,7 @@ describe('ContactUsPageComponent', () => {
   let component: ContactUsPageComponent;
   let fixture: ComponentFixture<ContactUsPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ContactUsPageComponent ]
     })
--- src/app/components/shopping-page/shopping-page.component.spec.ts
+++ src/app/components/shopping-page/shopping-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ShoppingPageComponent } from './shopping-page.component';
 
@@ -6,7 +6,7 @@ describe('ShoppingPageComponent', () => {
   let component: ShoppingPageComponent;
   let fixture: ComponentFixture<ShoppingPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ShoppingPageComponent ]
     })
