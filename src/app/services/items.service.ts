2374c92e36c36643fa17ca5eccc8c437a4fc53cf
Linus <vitocorleone77@gmail.com>
2020-12-06 19:22:31 +0100

    local json instead of Microsoft Service

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -2,10 +2,10 @@ import { Injectable, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Category, ICategory } from '../models/category';
 import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
 import { IItem, Item } from '../models/item';
 import { ISubcategory, Subcategory } from '../models/subcategory';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 
 
@@ -29,16 +29,16 @@ export class ItemsService implements OnInit {
   getItems(subcategoryNameExternal: string): Observable<Array<ICategory>> {
 
     // rubric81 : Data was accessed using the Azure Web API and not a local file
-    const configUrl = 'https://webmppcapstone.blob.core.windows.net/data/itemsdata.json'; // load online json with real imagelinks
+    let configUrl = 'https://webmppcapstone.blob.core.windows.net/data/itemsdata.json'; // load online json with real imagelinks
 
     // Disabled for submitting to Microsoft
-    // if (!environment.production) { // dev?
-    //   configUrl = '../assets/itemsdata.json'; // load local file with non-existing imagelinks due to huge data volume
-    // } else {
+    if (!environment.production) { // dev?
+      configUrl = '../assets/itemsdata.json'; // load local file with non-existing imagelinks due to huge data volume
+    } else {
 
-    // // rubric81 : Data was accessed using the Azure Web API and not a local file
-    //   configUrl = 'https://webmppcapstone.blob.core.windows.net/data/itemsdata.json'; // load online json with real imagelinks
-    // }
+    // rubric81 : Data was accessed using the Azure Web API and not a local file
+      configUrl = 'https://webmppcapstone.blob.core.windows.net/data/itemsdata.json'; // load online json with real imagelinks
+    }
 
     // rubric80 : Used jQuery or Angular for data binding
     return this.http.get<ICategory[]>(configUrl).pipe(
